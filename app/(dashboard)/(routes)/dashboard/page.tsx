b330b605856aebf27bca6781c85d81aa43d4631f
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-12-13 20:57:16 +0600

    convertation end

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,53 @@
 "use client";
 
+import { MessageSquare, ImageIcon, Code, VideoIcon, Music } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { ArrowRight } from "lucide-react";
 
 import { Card } from "@/components/ui/card";
-import { tools } from "@/constants";
 import { cn } from "@/lib/utils";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+
+  {
+    label: "Music generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music",
+  },
+
+  {
+    label: "Image generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+
+  {
+    label: "Video generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video",
+  },
+
+  {
+    label: "Code generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code",
+  },
+];
 
 const DashboardPage = () => {
   const router = useRouter();
