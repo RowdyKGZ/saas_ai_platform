db770169178975891ccd6c3a249ee2753e727081
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-12-14 19:02:18 +0600

    video generate

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -4,7 +4,7 @@ import z from "zod";
 import axios from "axios";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Music } from "lucide-react";
+import { Video } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -17,9 +17,9 @@ import { Loader } from "@/components/loader";
 
 import { formSchema } from "./constants";
 
-const MusicPage = () => {
+const VideoPage = () => {
   const router = useRouter();
-  const [music, setMusic] = useState<string>();
+  const [video, setVideo] = useState<string>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -32,11 +32,11 @@ const MusicPage = () => {
 
   const onSumbit = async (values: z.infer<typeof formSchema>) => {
     try {
-      setMusic(undefined);
+      setVideo(undefined);
 
-      const response = await axios.post("/api/music", values);
+      const response = await axios.post("/api/video", values);
 
-      setMusic(response.data.audio);
+      setVideo(response.data[0]);
       form.reset();
     } catch (error) {
       console.log(error);
@@ -48,11 +48,11 @@ const MusicPage = () => {
   return (
     <div>
       <Heading
-        title="Music generation"
-        description="Turn your prompt into music."
-        icon={Music}
-        iconColor="text-emerald-500"
-        bgColor="bg-emerald-500/10"
+        title="Video generation"
+        description="Turn your prompt into video."
+        icon={Video}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
       />
 
       <div className="px-4 lg:px-8">
@@ -70,7 +70,7 @@ const MusicPage = () => {
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="A hourse in swiss alpines"
+                        placeholder="Clown fish swimming around a coral reef"
                         {...field}
                       />
                     </FormControl>
@@ -95,12 +95,15 @@ const MusicPage = () => {
             </div>
           )}
 
-          {!music && !isLoading && <Empty label="No music generated" />}
+          {!video && !isLoading && <Empty label="No video generated" />}
 
-          {music && (
-            <audio controls className="w-full mt-8">
-              <source src={music} />
-            </audio>
+          {video && (
+            <video
+              controls
+              className="w-full aspect-video mt-8 rounded-lg border bg-black"
+            >
+              <source src={video} />
+            </video>
           )}
         </div>
       </div>
@@ -108,4 +111,4 @@ const MusicPage = () => {
   );
 };
 
-export default MusicPage;
+export default VideoPage;
